8b5909629406466df093b81a0e071f799fbbfd62
Marcos Ramalho <marcosrmlho@gmail.com>
2024-10-14 20:22:35 -0300

    Modificação no SQL
    Tentativa falha de criar o esquema do BD no MySQL pelo PHP, mesmo que pelo próprio SGBD tenha sido um sucesso. Código de SQL está estruturalmente correto, aparentemente.

--- esquemaBanco.sql
+++ esquemaBanco.sql
@@ -1,34 +1,30 @@
-create table Usuario(
-	CPF char(11) primary key,
-	nome varchar(40) not null,
-	dataNasc date not null
-)
-	
-create table Guia(
-	usuarioCPF char(11) primary key,
-	habilitacaoGuia char(14) not null,
-	foreign key (usuarioCPF) references Usuario(CPF)
-)
-
-create table Cliente(
-	usuarioCPF char(11) primary key,
-	foreign key (usuarioCPF) references Usuario(CPF)
-)
-
 create table Passeio(
-	idPasseio serial primary key,
+	idPasseio int(255) primary key,
 	HoraInicio time not null,
 	dataInicio date not null,
 	HoraFinal time not null,
 	dataFinal date not null,
 	nome varchar(60) not null,
-	ranking varchar(2) not null,
-	valor real not null,
-	dataCompra date not null,
-	tipoPagamento varchar(7) not null,
-	clienteCPF char(11) not null,
-	foreign key (clienteCPF) references Cliente(usuarioCPF)
-)
+	ranking int(2) not null,
+	valor double(10,2) not null
+);
+
+create table Usuario(
+	CPF char(11) primary key,
+	nome varchar(40) not null,
+	dataNasc date not null
+);
+	
+create table Guia(
+	usuarioCPF char(11) primary key,
+	habilitacaoGuia char(14) not null,
+	foreign key (usuarioCPF) references Usuario(CPF)
+);
+
+create table Cliente(
+	usuarioCPF char(11) primary key,
+	foreign key (usuarioCPF) references Usuario(CPF)
+);
 
 create table DadosCartao(
 	numero varchar(16) primary key,
@@ -36,12 +32,21 @@ create table DadosCartao(
 	agencia varchar(5) not null,
 	clienteCPF char(11) not null,
 	foreign key (clienteCPF) references Cliente(usuarioCPF)
-)
+);
+
+create table Comprar (
+	clienteCPF char(11),
+	idPasseio int(255),
+	dataCompra date not null,
+	tipoPagamento varchar(7) not null,
+	foreign key (clienteCPF) references Cliente(usuarioCPF),
+	foreign key (idPasseio) references Passeio(idPasseio)
+);
 
 create table Guiar(
 	guiaCPF char(11),
-	idPasseio serial,
+	idPasseio int(255),
 	primary key(guiaCPF, idPasseio),
 	foreign key (guiaCPF) references Guia(usuarioCPF),
 	foreign key (idPasseio) references Passeio(idPasseio)
-)
+);
--- static/scripts/banco.php
+++ static/scripts/banco.php
@@ -4,18 +4,71 @@ $username = "root";
 $password = "";
 $dbname = "trabalhoFinal";
 
-
+// Criar a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Verificar a conexão
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-//inserir sql pro mysql (é meio diferente do postgres...)
-$sql = "";
 
-/*
+$sql = "
+create table Passeio(
+	idPasseio int(11) primary key,
+	HoraInicio time not null,
+	dataInicio date not null,
+	HoraFinal time not null,
+	dataFinal date not null,
+	nome varchar(60) not null,
+	ranking varchar(2) not null,
+	valor double(10,2) not null
+);
+
+create table Usuario(
+	CPF char(11) primary key,
+	nome varchar(40) not null,
+	dataNasc date not null
+);
+	
+create table Guia(
+	usuarioCPF char(11) primary key,
+	habilitacaoGuia char(14) not null,
+	foreign key (usuarioCPF) references Usuario(CPF)
+);
+
+create table Cliente(
+	usuarioCPF char(11) primary key,
+	foreign key (usuarioCPF) references Usuario(CPF)
+);
+
+create table DadosCartao(
+	numero varchar(16) primary key,
+	CVV varchar(4) not null,
+	agencia varchar(5) not null,
+	clienteCPF char(11) not null,
+	foreign key (clienteCPF) references Cliente(usuarioCPF)
+);
+
+create table Comprar (
+	clienteCPF char(11),
+	idPasseio int(11),
+	dataCompra date not null,
+	tipoPagamento varchar(7) not null,
+	foreign key (clienteCPF) references Cliente(usuarioCPF),
+	foreign key (idPasseio) references Passeio(idPasseio)
+);
+
+create table Guiar(
+	guiaCPF char(11),
+	idPasseio int(11),
+	primary key(guiaCPF, idPasseio),
+	foreign key (guiaCPF) references Guia(usuarioCPF),
+	foreign key (idPasseio) references Passeio(idPasseio)
+);
+";
+
 if ($conn->query($sql) === FALSE) {
   echo "Error creating table: " . $conn->error;
-  }
-*/
+}
+
 ?>
